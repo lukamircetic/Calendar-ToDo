4e147865b0c14cea35e990c2124f28022eb77ef6
Luka Mircetic <luka.mircetic@gmail.com>
2020-08-11 01:21:15 -0400

    Got a basic server running

---
+++ server/src/server.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import { json, urlencoded } from 'body-parser'
+import morgan from 'morgan'
+import config from './config'
+import cors from 'cors'
+import { connect } from './utils/db'
+export const app = express()
+
+app.disable('x-powered-by')
+app.use(cors())
+app.use(json())
+app.use(urlencoded({ extended: true }))
+app.use(morgan('dev'))
+
+export const start = async () => {
+  try {
+    await connect()
+    app.listen(config.port, () => {
+      console.log(`REST API on http://:${config.port}/api`)
+    })
+  } catch (e) {
+    console.error(e)
+  }
+}
