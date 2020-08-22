8f8ce61405a1d0024efb24efe39624685d8e3d9a
Luka Mircetic <luka.mircetic@gmail.com>
2020-08-22 19:22:40 -0400

    Added a helper for get requests

--- client/src/helpers.js
+++ client/src/helpers.js
@@ -1,13 +1,35 @@
+import { current_user_id, user_token } from './states'
+
+let token
+
+const unsubscribe = user_token.subscribe((value) => {
+  token = value
+})
 export async function fetchPost(url, data) {
-    const res = await fetch(
-        url, {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            },
-            body: JSON.stringify(data),
-        });
-        const response = await res.json();
-        return response;
+  const res = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      authorization: `Bearer ${token}`,
+    },
+    user: { _id: current_user_id },
+    body: JSON.stringify(data),
+  })
+  const response = await res.json()
+  return response
+}
+
+export async function fetchGet(url) {
+  const res = await fetch(url, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      authorization: `Bearer ${token}`,
+    },
+    user: { _id: current_user_id },
+  })
+  const response = await res.json()
+  return response
 }
